f641343962b4c7ba714dc8e23ed54fbfbfb77528
Bafana <76040794+Bafana29@users.noreply.github.com>
2021-03-22 09:45:07 +0530

    Add files via upload

---
+++ chat_room.js
@@ -0,0 +1,14 @@
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyCKCqd9azYj66cG8c3Nk1f3bdtokCJjD68",
+    authDomain: "lets-chat-3fb20.firebaseapp.com",
+    projectId: "lets-chat-3fb20",
+    storageBucket: "lets-chat-3fb20.appspot.com",
+    messagingSenderId: "788206601390",
+    appId: "1:788206601390:web:e14d94f40911fffc62193f",
+    measurementId: "G-KDEP59NJKK"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
